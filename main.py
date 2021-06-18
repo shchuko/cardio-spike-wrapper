bfa6c1354e8bce42b90396a4f09d8290eeb1ac97
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-06-18 20:59:45 +0300

    main: implement stubbed endpoints

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
-from typing import Optional
+import io
+from tempfile import NamedTemporaryFile
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, HTTPException
+from fastapi.responses import FileResponse, StreamingResponse
+
+from detect.analyzers import BaseAnalyzer
+from detect.stubs import AnalyzerStub
 
 app = FastAPI()
+response_file = NamedTemporaryFile("w+b")
+analyzer = AnalyzerStub()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "Worldd"}
+@app.post("/analyze")
+async def load_file(file: bytes = File(...)):
+    result_id = analyzer.queue_analyze(file.decode())
+    return {"result_id": result_id}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/result")
+async def get_result_csv(result_id: str):
+    if analyzer.is_ready(result_id=result_id) != BaseAnalyzer.AnalyzeStatus.READY:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    csv_path = analyzer.get_result(result_id=result_id).get_csv_path()
+    return FileResponse(path=csv_path, filename="cardio-result.csv")
+
+
+@app.get("/stats")
+async def get_stats(result_id: str):
+    if analyzer.is_ready(result_id=result_id) != BaseAnalyzer.AnalyzeStatus.READY:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    stats = analyzer.get_result(result_id=result_id).get_stats()
+    return stats
+
+
+@app.get("/diagram")
+async def get_diagram(result_id: str):
+    if analyzer.is_ready(result_id=result_id) != BaseAnalyzer.AnalyzeStatus.READY:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    png_bytes = analyzer.get_result(result_id=result_id).get_png()
+    return StreamingResponse(io.BytesIO(png_bytes), media_type="image/png")
+
+
+@app.get("/status")
+async def check_status(result_id: str):
+    status = "not_found"
+    if analyzer.is_ready(result_id=result_id) == BaseAnalyzer.AnalyzeStatus.READY:
+        status = "ready"
+    elif analyzer.is_ready(result_id=result_id) == BaseAnalyzer.AnalyzeStatus.PROCESSING:
+        status = "processing"
+
+    return {"status": status}
