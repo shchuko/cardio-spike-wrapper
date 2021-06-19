a138664feb6f02b1433f30be312a44d112265b5b
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-06-19 04:44:10 +0300

    BaseAnalyzer: drop dedundant AnalyzeStatus

--- detect/analyzers.py
+++ detect/analyzers.py
@@ -1,12 +1,6 @@
 from detect.result import BaseResult
-from enum import Enum
 
 
 class BaseAnalyzer:
-    class AnalyzeStatus(Enum):
-        PROCESSING = 1
-        READY = 2
-        NOT_FOUND = 3
-
     async def analyze(self, csv_content: str) -> BaseResult:
         pass
